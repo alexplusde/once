41045786fb0696faaffd24bf58270c11fd024db5
Alexander Walther <aw@alexplus.de>
2022-07-19 15:09:32 +0200

    codemirror, test input

--- pages/index.php
+++ pages/index.php
@@ -4,8 +4,12 @@ $addon = rex_addon::get('once');
 
 $form = rex_config_form::factory($addon->name);
 
-$field = $form->addTextAreaField('input', null, ["class" => "form-control"]);
+$field = $form->addTextAreaField('input', null, ["class" => "form-control codemirror", "data-codemirror-theme" =>"themename", "data-codemirror-mode" => "php/htmlmixed"]);
 $field->setLabel(rex_i18n::msg('once_input_label'));
+
+if ($input = rex_post("input")) {
+    dump($input);
+}
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
