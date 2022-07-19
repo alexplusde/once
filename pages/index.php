44d7d04db97265cb29a7ed6690cba6f397fc98c9
Alexander Walther <aw@alexplus.de>
2022-07-19 15:07:38 +0200

    update file path

--- pages/index.php
+++ pages/index.php
@@ -1,1 +1,14 @@
 <?php
+#
+$addon = rex_addon::get('once');
+
+$form = rex_config_form::factory($addon->name);
+
+$field = $form->addTextAreaField('input', null, ["class" => "form-control"]);
+$field->setLabel(rex_i18n::msg('once_input_label'));
+
+$fragment = new rex_fragment();
+$fragment->setVar('class', 'edit', false);
+$fragment->setVar('title', $addon->i18n('once_config'), false);
+$fragment->setVar('body', $form->get(), false);
+echo $fragment->parse('core/page/section.php');
